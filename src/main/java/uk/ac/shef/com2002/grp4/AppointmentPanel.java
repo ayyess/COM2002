eea0689b0e2803d14061356d2710f3aee1ae8336
Andrew Scott <ascott2@sheffield.ac.uk>
2016-11-02 14:18:36 +0000

    Add JDatePicker to appointment pane

--- src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
+++ src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
@@ -2,12 +2,61 @@ package uk.ac.shef.com2002.grp4;
 
 import java.awt.*;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+import java.util.Calendar;
+import java.util.Date;
+import org.jdatepicker.impl.*;
+import org.jdatepicker.util.*;
+import org.jdatepicker.*;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 /**
  * Panel for appointment interaction workflow
  *
  * Created on 28/10/2016.
  */
-public class AppointmentPanel extends Component {
-    //TODO searching appointments
-    //TODO creating appointments
+public class AppointmentPanel extends JPanel {
+	public class DateLabelFormatter extends AbstractFormatter {
+
+		private String datePattern = "yyyy-MM-dd";
+		private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+
+		@Override
+		public Object stringToValue(String text) throws ParseException {
+			return dateFormatter.parseObject(text);
+		}
+
+		@Override
+		public String valueToString(Object value) throws ParseException {
+			if (value != null) {
+				Calendar cal = (Calendar) value;
+				return dateFormatter.format(cal.getTime());
+			}
+
+			return "";
+		}
+
+	}
+	//TODO searching appointments
+	//TODO creating appointments
+	public AppointmentPanel () {
+		UtilDateModel model = new UtilDateModel();
+		//model.setDate(20,04,2014);
+		// Need this...
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		// Don't know about the formatter, but there it is...
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+		this.add(datePicker);
+		add(new JLabel("Hello World"));
+	}
 }
