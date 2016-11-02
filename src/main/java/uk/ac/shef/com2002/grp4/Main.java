b6d88352fde9da07fdc4c84ebcb711e06dcebb15
Euan Rochester <euan@rochester.me.uk>
2016-11-02 13:21:29 +0000

    fixed tabs?

--- src/main/java/uk/ac/shef/com2002/grp4/Main.java
+++ src/main/java/uk/ac/shef/com2002/grp4/Main.java
@@ -5,43 +5,44 @@ import java.awt.*;
 
 public class Main extends JFrame {
 
-	public Main() {
-		//TODO add a practitioner ui that can be switched out for use in the treatment rooms
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setPreferredSize(new Dimension(800,600));
-		pack();
-		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.addTab("Appointments", null, new AppointmentPanel(), null);
-		tabbedPane.addTab("Customers", null, new CustomerPanel(), null);
-		tabbedPane.addTab("Payment", null, new PaymentPanel(), null);
-		add(tabbedPane);
-		setVisible(true);
-	}
+    public Main() {
+        //TODO add a practitioner ui that can be switched out for use in the treatment rooms
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setPreferredSize(new Dimension(800,600));
+        pack();
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Appointments", null, new AppointmentPanel(), null);
+        tabbedPane.addTab("Customers", null, new CustomerPanel(), null);
+        tabbedPane.addTab("Payment", null, new PaymentPanel(), null);
+        add(tabbedPane);
+        setVisible(true);
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(
-						UIManager.getSystemLookAndFeelClassName());
-					//hack for dpi scaling, really only because 4k screens are annoying
-					float dpiScaling =Toolkit.getDefaultToolkit().getScreenResolution()/86;
-					for(Object key: UIManager.getLookAndFeelDefaults().keySet()){
-												if(key != null && key.toString().toLowerCase().contains("font")){
-														Font font = UIManager.getDefaults().getFont(key);
-														if(font != null){
-																int size = font.getSize();
-																float scaledSize = size*dpiScaling;
-																font = font.deriveFont(scaledSize);
-																UIManager.put(key,font);
-														}
-												}
-				} catch (Exception e) {
-					//we can ignore any exceptions here as they won't change the functionality of the program, it just won't look native
-				}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName());
+                    //hack for dpi scaling, really only because 4k screens are annoying
+                    float dpiScaling =Toolkit.getDefaultToolkit().getScreenResolution()/86;
+                    for(Object key: UIManager.getLookAndFeelDefaults().keySet()) {
+                        if(key != null && key.toString().toLowerCase().contains("font")) {
+                            Font font = UIManager.getDefaults().getFont(key);
+                            if(font != null) {
+                                int size = font.getSize();
+                                float scaledSize = size*dpiScaling;
+                                font = font.deriveFont(scaledSize);
+                                UIManager.put(key,font);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    //we can ignore any exceptions here as they won't change the functionality of the program, it just won't look native
+                }
 
-				new Main();
-			}
-		});
-	}
+                new Main();
+            }
+        });
+    }
 }
--- src/main/java/uk/ac/shef/com2002/grp4/PaymentPanel.java
+++ src/main/java/uk/ac/shef/com2002/grp4/PaymentPanel.java
@@ -8,8 +8,8 @@ import java.awt.*;
  * Created on 28/10/2016.
  */
 public class PaymentPanel extends Component {
-	//TODO searching outstanding payments
-	//TODO printing outstanding payments
-	//thoughts: payments could potentially be reachable via the customer record, should
-	//this panel instead be integrated into that one somehow?
+    //TODO searching outstanding payments
+    //TODO printing outstanding payments
+    //thoughts: payments could potentially be reachable via the customer record, should
+    //this panel instead be integrated into that one somehow?
 }
