f9235409d2f3aec20000c69b010a59c2b19959d4
Andrew Scott <ascott2@sheffield.ac.uk>
2016-11-02 15:37:27 +0000

    Add hour view to appointment panel

--- src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
+++ src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
@@ -15,6 +15,11 @@ import org.jdatepicker.*;
 import javax.swing.JFormattedTextField.AbstractFormatter;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import uk.ac.shef.com2002.grp4.calendar.CalendarComp;
+import uk.ac.shef.com2002.grp4.calendar.Appointment;
 
 /**
  * Panel for appointment interaction workflow
@@ -49,11 +54,44 @@ public class AppointmentPanel extends JPanel {
 		UtilDateModel model = new UtilDateModel();
 		//model.setDate(20,04,2014);
 		// Need this...
-		Properties p = new Properties();
-		p.put("text.today", "Today");
-		p.put("text.month", "Month");
-		p.put("text.year", "Year");
-		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-		this.add(datePanel);
+		{
+			Properties p = new Properties();
+			p.put("text.today", "Today");
+			p.put("text.month", "Month");
+			p.put("text.year", "Year");
+			JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+			datePanel.getModel().addChangeListener(new ChangeListener() {
+					public void stateChanged(ChangeEvent e) {
+						UtilDateModel source = (UtilDateModel) e.getSource();
+						System.out.println("here");
+						System.out.println(source.getValue());
+						// AppointmentPanel.this.updateDat
+					}
+				});
+			this.add(datePanel ,BorderLayout.LINE_START);
+		}
+		//this.add(new JLabel ("Hello") ,BorderLayout.LINE_END);
+
+		{
+			JScrollPane p = new JScrollPane();
+			CalendarComp c = new CalendarComp();
+			Calendar calS = Calendar.getInstance();
+			Calendar calE = Calendar.getInstance();
+			calS.set(2016, 11, 2, 10, 40, 0);
+			calE.set(2016, 11, 2, 11, 40, 0);
+			c.addAppointment(new Appointment(calS,calE));
+			calS.set(2016, 11, 2, 16, 0, 0);
+			calE.set(2016, 11, 2, 16, 40, 0);
+			c.addAppointment(new Appointment(calS,calE));
+			calS.set(2016, 11, 2, 20, 0, 0);
+			calE.set(2016, 11, 2, 21, 40, 0);
+			c.addAppointment(new Appointment(calS,calE));
+			p.setVerticalScrollBarPolicy(p.VERTICAL_SCROLLBAR_ALWAYS);
+			//p.add(new JLabel("hellow world"));
+			p.getViewport().add(c);
+
+			p.setPreferredSize(new Dimension(200, 600));
+			this.add(p, BorderLayout.LINE_END);
+		}
 	}
 }
