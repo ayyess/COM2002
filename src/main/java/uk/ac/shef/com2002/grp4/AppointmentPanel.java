f3a8f4ec0a756b2d75de3f99f82d3796e442688a
Euan Rochester <euan@rochester.me.uk>
2016-11-09 22:47:38 +0000

    More fixing for sloppy merge

--- src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
+++ src/main/java/uk/ac/shef/com2002/grp4/AppointmentPanel.java
@@ -26,27 +26,6 @@ import uk.ac.shef.com2002.grp4.util.DPIScaling;
  * Created on 28/10/2016.
  */
 public class AppointmentPanel extends JPanel {
-	public class DateLabelFormatter extends AbstractFormatter {
-
-		private String datePattern = "yyyy-MM-dd";
-		private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
-
-		@Override
-		public Object stringToValue(String text) throws ParseException {
-			return dateFormatter.parseObject(text);
-		}
-
-		@Override
-		public String valueToString(Object value) throws ParseException {
-			if (value != null) {
-				Calendar cal = (Calendar) value;
-				return dateFormatter.format(cal.getTime());
-			}
-
-			return "";
-		}
-
-	}
 	//TODO searching appointments
 	//TODO creating appointments
 	public AppointmentPanel () {
