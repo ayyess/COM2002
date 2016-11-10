d6ce11aa201ef35e892b0991eac51d55f33a389f
Euan Rochester <euan@rochester.me.uk>
2016-11-10 16:06:08 +0000

    More work on patient creation

--- src/main/java/uk/ac/shef/com2002/grp4/CreatePatientDialog.java
+++ src/main/java/uk/ac/shef/com2002/grp4/CreatePatientDialog.java
@@ -1,40 +1,94 @@
 package uk.ac.shef.com2002.grp4;
 
 import javax.swing.*;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Container;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.util.Calendar;
+
 import org.jdatepicker.*;
+import uk.ac.shef.com2002.grp4.databases.PatientUtils;
 import uk.ac.shef.com2002.grp4.util.DPIScaling;
 
-public class CreatePatientDialog extends JDialog{
+public class CreatePatientDialog extends JDialog implements ActionListener {
+	private int row = 0;
+	private JButton createButton;
+	private JTextField titleField;
+	private JTextField forenameField;
+	private JTextField surnameField;
+	private JDatePicker dobField;
+	private JTextField phoneField;
+
+	protected GridBagConstraints getBaseConstraints(){
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5,5,5,5);
+		c.gridy=row;
+		return c;
+	}
+	protected void addLabeledInput(String label,JComponent input){
+		Container contentPane = rootPane.getContentPane();
+		GridBagConstraints c = getBaseConstraints();
+
+		c.fill=GridBagConstraints.HORIZONTAL;
+		c.anchor=GridBagConstraints.EAST;
+		contentPane.add(new JLabel(label+":"),c);
+		c.anchor=GridBagConstraints.WEST;
+		contentPane.add(input,c);
+
+		row++;
+	}
 	public CreatePatientDialog(JFrame owner){
 		super(owner,"Create Patient",false);
 		Container contentPane = rootPane.getContentPane();
 		contentPane.setLayout(new GridBagLayout());
 
-		GridBagConstraints c = new GridBagConstraints();
+		titleField = new JTextField();
+		forenameField = new JTextField();
+		surnameField = new JTextField();
+		dobField = new JDatePicker();
+		phoneField = new JTextField();
+		phoneField.setInputVerifier(new InputVerifier() {
+			@Override
+			public boolean verify(JComponent input) {
+				JTextField field = (JTextField)input;
+				return field.getText().matches("^[0-9 \\-]+$");
+			}
+		});
+		addLabeledInput("Title",titleField);
+		addLabeledInput("Forename",forenameField);
+		addLabeledInput("Surname",surnameField);
+		addLabeledInput("DoB",dobField);
+		addLabeledInput("Phone number",phoneField);
 
-		c.gridy+=1;
-		contentPane.add(new JLabel("Title"),c);
-		contentPane.add(new JTextField(5),c);
-		
-		c.gridy+=1;
-		contentPane.add(new JLabel("Forename"),c);
-		contentPane.add(new JTextField(10),c);
+		GridBagConstraints c = getBaseConstraints();
 
-		c.gridy+=1;
-		contentPane.add(new JLabel("Surname"),c);
-		contentPane.add(new JTextField(10),c);
+		c.gridwidth=2;
+		createButton = new JButton("Create");
+		createButton.addActionListener(this);
+		contentPane.add(createButton,c);
 
-		int dpiScaling = (int)DPIScaling.get();
-		JDatePicker datePicker = new JDatePicker();
-		JDatePanel datePanel  = (JDatePanel) datePicker.getJDateInstantPanel();
-		datePanel.setPreferredSize(new java.awt.Dimension(200*dpiScaling,180*dpiScaling));
+		row++;
 
-		c.gridy+=1;
-		contentPane.add(new JLabel("DoB"),c);
-		contentPane.add(new JDatePicker(),c);
+		pack();
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == createButton){
+			String title = titleField.getText();
+			String forename = forenameField.getText();
+			String surname = surnameField.getText();
+			Calendar cal = (Calendar) dobField.getModel().getValue();
+			if(cal == null){
+				JOptionPane.showMessageDialog(this,"You must select a date","Validation error",JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			System.out.println(cal);
+			Date dob = new Date(cal.getTimeInMillis());
+			String phoneNumber = phoneField.getText();
+			int addressId = -1;//FIXME need to provide a way to search for, or create an address
+			PatientUtils.insertPatient(title,forename,surname,dob,phoneNumber,addressId);
+		}
 	}
 }
