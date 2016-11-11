5294af5c0f7f2f8f4a795c888b15e34eca6d4db3
Andrew Scott <ascott2@sheffield.ac.uk>
2016-11-11 13:45:59 +0000

    Rename sql tables

--- up.sql
+++ up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE address(
+CREATE TABLE IF NOT EXISTS addresses (
     id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
     house_number BIGINT NOT NULL,
     street VARCHAR(255) NOT NULL,
@@ -8,20 +8,20 @@ CREATE TABLE address(
     PRIMARY KEY ( id )
 );
 
-CREATE TABLE patient(
+CREATE TABLE IF NOT EXISTS patients (
     id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL, -- SERIAL is an alias for BIGINT UNSIGNED AUTOINCREMENT NOT NULL
     title VARCHAR(5) NOT NULL, -- A maxium of 5 characters for a title seems reasonable
-    forname VARCHAR(255) NOT NULL, -- A maxium of 255 characters for a forname seems reasonable
-    surname VARCHAR(255) NOT NULL, -- A maxium of 255 characters for a surname seems reasonable
+    first_name VARCHAR(255) NOT NULL, -- A maxium of 255 characters for a first_name seems reasonable
+    last_name VARCHAR(255) NOT NULL, -- A maxium of 255 characters for a last_name seems reasonable
     dob DATE NOT NULL,
     phone_number VARCHAR(15) NOT NULL, -- 15 is the maximum length in theory: https://en.wikipedia.org/wiki/Telephone_numbering_plan#International_numbering_plan
     address_id BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY ( id ),
     FOREIGN KEY (address_id)
-        REFERENCES address(id)
+        REFERENCES addresses(id)
 );
 
-CREATE TABLE appointment(
+CREATE TABLE IF NOT EXISTS appointments (
     date DATE NOT NULL,
     practitioner VARCHAR(255) NOT NULL,
     patient_id BIGINT UNSIGNED, -- this is allowed to be null so that balnk appointments can be booked, as specified
@@ -29,36 +29,37 @@ CREATE TABLE appointment(
     duration INT NOT NULL,
     PRIMARY KEY(date,practitioner,start),
     FOREIGN KEY(patient_id)
-        REFERENCES patient(id)
+        REFERENCES patients(id)
 );
 
-CREATE TABLE treatment(
+CREATE TABLE IF NOT EXISTS treatments (
     name VARCHAR(255) NOT NULL,
     cost BIGINT UNSIGNED NOT NULL,
-    treatment_type ENUM('CHECKUP','HYGIENE','REPAIR')
+    treatment_type ENUM('CHECKUP','HYGIENE','REPAIR'),
     PRIMARY KEY(name)
 );
 
-CREATE TABLE treatment_applications(
+CREATE TABLE IF NOT EXISTS treatment_applications(
     treatment_name VARCHAR(255) NOT NULL,
     appointment_date DATE NOT NULL,
     practitioner VARCHAR(255) NOT NULL,
     PRIMARY KEY(treatment_name,appointment_date,practitioner),
     FOREIGN KEY(treatment_name)
-        REFERENCES treatment(name),
+        REFERENCES treatments(name),
     FOREIGN KEY(appointment_date,practitioner)
-        REFERENCES appointment(date,practitioner)
+        REFERENCES appointments(date,practitioner)
 );
 
-CREATE TABLE treatment_plan(
+CREATE TABLE IF NOT EXISTS treatment_plans (
     name VARCHAR(255) NOT NULL,
     cost BIGINT UNSIGNED NOT NULL,
     checkups INT UNSIGNED NOT NULL,
     hygiene_visits INT UNSIGNED NOT NULL,
-    repairs INT UNSIGNED NOT NULL
+    repairs INT UNSIGNED NOT NULL,
+    PRIMARY KEY(name)
 );
 
-CREATE TABLE patient_plan(
+CREATE TABLE IF NOT EXISTS patient_plans (
     patient_id BIGINT UNSIGNED NOT NULL,
     plan_name VARCHAR(255) NOT NULL,
     used_checkups INT UNSIGNED NOT NULL,
@@ -66,7 +67,7 @@ CREATE TABLE patient_plan(
     used_repairs INT UNSIGNED NOT NULL,
     PRIMARY KEY(patient_id,plan_name),
     FOREIGN KEY(patient_id)
-        REFERENCES patient(id),
+        REFERENCES patients(id),
     FOREIGN KEY(plan_name)
-        REFERENCES treatment_plan(name)
+        REFERENCES treatment_plans(name)
 );
