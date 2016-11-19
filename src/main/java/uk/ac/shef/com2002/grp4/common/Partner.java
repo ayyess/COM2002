6a56da4593fee94b74a1010c356d4e7df40d4862
Dan-L <djreynolds1@sheffield.ac.uk>
2016-11-19 11:05:46 +0000

    Added updated license header

--- src/main/java/uk/ac/shef/com2002/grp4/common/Partner.java
+++ src/main/java/uk/ac/shef/com2002/grp4/common/Partner.java
@@ -1,3 +1,11 @@
+/* This file is part of Grp4 Dental Care System.
+ * This system is for private, educational use. It should solely be viewed by those
+ * marking the COM2002 assignment.
+ * Unauthorised copying or editing of this file is strictly prohibited.
+ *
+ * This system uses GPL-licensed software.
+ * Visit <http://www.gnu.org/licenses/> to see the license.
+ */
 package uk.ac.shef.com2002.grp4.common;
 
 public enum Partner {
